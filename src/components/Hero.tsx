cbe564c66efba708a97fca18c90a611348d2ae42
Lucas Moraes <lucasmo066@gmail.com>
2024-05-07 17:50:17 -0400

    message

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ export function Hero() {
   const images = ["/hero.jpeg"];
 
   return (
-    <ImagesSlider className="h-[60rem]" images={images}>
+    <ImagesSlider className="h-[70rem]" images={images}>
       <motion.div
         initial={{
           opacity: 0,
@@ -58,9 +58,9 @@ export function Hero() {
             transition={{
               duration: 0.5,
             }}
-            className="py-0.5 px-4 border-2 border-black dark:border-white uppercase bg-white text-neutral-700 transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+            className="py-0.5 px-4 border-2 border-black dark:border-zinc-300 bg-zinc-300 text-neutral-700 transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
           >
-            Order Now
+            See Menu
           </motion.button>
           <motion.button
             initial={{
@@ -74,7 +74,7 @@ export function Hero() {
             transition={{
               duration: 0.5,
             }}
-            className="py-0.5 px-4 border-2 border-black dark:border-white uppercase bg-white text-neutral-700 transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+            className="py-0.5 px-4 border-2 border-zinc-300 dark:border-zinc-300 bg-zinc-300 text-neutral-700 transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
           >
             Order Now
           </motion.button>
