76c2ec1cd86bcc420ff24426c7aae2505f72b99f
Lucas Moraes <lucasmo066@gmail.com>
2024-05-08 18:00:20 -0400

    leave reviews

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -1,10 +1,17 @@
 import { InfiniteMovingCardsDemo } from "@/components/MovingCards";
+import { Button } from "@/components/ui/button";
 
 export default function TestimonialsSection() {
     return (
         <div className="flex flex-col items-center w-full border-4 border-yellow-500">
             <h2 className="text-3xl font-extralight text-primary mt-10">What our customers are saying</h2>
             <InfiniteMovingCardsDemo />
+            <div className="flex flex-col border-4 items-center justify-center border-green-500 w-full">
+                <h3 className="text-xl font-semibold pb-5">
+                Leave us a review on Yelp!
+                </h3>
+                <Button>Go to Yelp</Button>
+            </div>
         </div>
     );
 }
