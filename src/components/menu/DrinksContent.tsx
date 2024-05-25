b6863a224a82c889152bf5df03d101f9f9b669ea
Lucas Moraes <lucasmo066@gmail.com>
2024-05-24 23:29:15 -0400

    drinks menu done

--- src/components/menu/DrinksContent.tsx
+++ src/components/menu/DrinksContent.tsx
@@ -1,9 +1,59 @@
+import DrinksContentSection from "@/components/menu/DrinksContentSection";
 
+const DrinksContent = () => {
+  return (
+    <div className="">
+      <DrinksContentSection
+        title= "Beverages"
+        sectionDescription= "Non-alcoholic beverages"
+        menuItems={[
+          { name: "Fountain Drinks or Iced Tea", price: "$2.00", description: "" },
+          { name: "Bottled Soda, Water, or Juices", price: "$2.00", description: "" },
+          { name: "Hot Coffee or Tea", price: "$2.00", description: "" },
+          { name: "Turkish Coffee", price: "$3.00", description: "Very strong coffee prepared the authentic way"},
+        ]}
+        variant= "left"
+        />
+        <DrinksContentSection
+        title= "White Wine"
+        imageUrl="/menu/winesection.jpg"
+        menuItems={[
+          { name: "Sterling Chardonnay", price: "8.00 / 29.00", description: "A rich and buttery white wine" },
+          { name: `Harken "Bottle Fermented" Chardonnay`, price: "9.00 / 33.00", description: "A rich and buttery white wine" },
+          { name: "The Champion Sauvignon Blanc", price: "$8.00 / $29.00", description: "A crisp and refreshing white wine" },
+          { name: "Beroni Fini Pinot Grigio", price: "$9.00 / $33.00", description: "A light and refreshing white wine" },
+          { name: "Stella Rosa Moscato", price: "$8.00 / $29.00", description: "A sweet and fruity white wine" },
+        ]}
+        variant= "left"
+        />
+        <DrinksContentSection
+            title= "Red Wine"
+            menuItems={[
+              { name: "Sterling Merlot", price: "$8.00 / $29.00", description: "A rich and fruity red wine" },
+              { name: "Sterling Cabernet Sauvignon", price: "$9.00 / $33.00", description: "A bold and full-bodied red wine" },
+              { name: "Sterling Pinot Noir", price: "$8.00 / $29.00", description: "A light and fruity red wine" },
+              { name: "Sterling Malbec", price: "$9.00 / $33.00", description: "A bold and spicy red wine" },
+              { name: "Sterling Shiraz", price: "$8.00 / $29.00", description: "A bold and spicy red wine" },
+            ]}
+            variant= "right"
+            />
+        <DrinksContentSection
+            title= "Beer"
+            sectionDescription= "Imported and domestic beers"
+            imageUrl="/menu/beerfridge.jpg"
+            menuItems={[
+              { name: "Bud Light", price: "$3.00", description: "" },
+              { name: "Miller Lite", price: "$3.00", description: "" },
+              { name: "Coors Light", price: "$3.00", description: "" },
+              { name: "Corona", price: "$4.00", description: "" },
+              { name: "Heineken", price: "$4.00", description: "" },
+              { name: "Stella Artois", price: "$4.00", description: "" },
+            ]}
+            variant= "right"
+            />
+  
+    </div>
+  );
+};
 
-export default function DrinksContent() {
-    return (
-        <div>
-            This is the Drinks menu
-        </div>
-    )
-}
+export default DrinksContent;
---
+++ src/components/menu/DrinksContentSection.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Image from "next/image";
+import classNames from "classnames";
+
+interface MenuItem {
+  name: string;
+  price: string;
+  description: string;
+}
+
+interface DrinksContentSectionsProps {
+  title: string;
+  sectionDescription?: string;
+  imageUrl?: string;
+  menuItems: MenuItem[];
+  variant: 'left' | 'right';
+}
+
+const FoodContentSections: React.FC<DrinksContentSectionsProps> = ({ title, sectionDescription, imageUrl, menuItems, variant }) => {
+  return (
+    <div className="border-b border-dotted border-neutral-600 pb-8 pt-6">
+      <h2 className="text-3xl font-semibold mb-2 text-start">{title}</h2>
+      {sectionDescription && <p className="text-neutral-400 text-md font-light mb-6">{sectionDescription}</p>}
+      <div className={classNames("flex flex-col md:flex-row md:items-center", { "md:flex-row-reverse": variant === 'right' })}>
+        {imageUrl && (
+          <div className="w-full md:w-1/2 mb-4 md:mb-0 md:h-auto">
+            <Image
+              src={imageUrl}
+              alt={title}
+              layout="responsive"
+              width={400}
+              height={300}
+              className="rounded-3xl border border-neutral-800 object-cover"
+            />
+          </div>
+        )}
+        <div className={`w-full md:w-${imageUrl ? '1/2' : 'full'} px-2 md:px-8`}>
+          <ul className="pl-6 py-4">
+            {menuItems.map((item, index) => (
+              <li key={index} className="mb-3">
+                <div className="flex justify-between">
+                  <span className="font-semibold">{item.name}</span>
+                  <span className="dark:text-white text-black font-extralight">{item.price}</span>
+                </div>
+                <div className="text-neutral-400 text-sm font-mono mb-2">{item.description}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodContentSections;
