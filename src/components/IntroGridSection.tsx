edb9b7e2a2270c067c89790191efa410ba9d1328
Lucas Moraes <lucasmo066@gmail.com>
2024-06-07 13:39:21 -0400

    margin on private events again fuck

--- src/components/IntroGridSection.tsx
+++ src/components/IntroGridSection.tsx
@@ -2,7 +2,7 @@ import { LayoutGridCards } from '@/components/LayoutGrid';
 
 export default function IntroGridSection() {
   return (
-    <div className="h-screen w-full my-20">
+    <div className="h-screen w-full mt-20 mb-40 lg:mb-20">
       <h2 className="aladin-regular text-center text-8xl md:text-9xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-red-600 to-red-950">Learn About Us</h2>
       <p className="text-xl font-extralight text-black dark:text-white pt-10 mx-5 md:mx-10">Click on the pictures below to learn more about us and our values</p>
       <LayoutGridCards />
